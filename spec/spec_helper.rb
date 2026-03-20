90b445b99e2330a3fe70b79f29fec2d945af7773
amadzai <20966134+amadzai@users.noreply.github.com>
2026-03-20 18:13:59 +0800

    feat: Setup rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+end
